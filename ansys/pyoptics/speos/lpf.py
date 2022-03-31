f38f85901223d15200daaee2f0bddfdba33b7538
Etienne Arnal <etienne.arnal@ansys.com>
2022-03-31 11:24:28 +0200

    fix flake8

--- ansys/pyoptics/speos/lpf.py
+++ ansys/pyoptics/speos/lpf.py
@@ -2,7 +2,7 @@
 import grpc
 import ansys.api.speos.results.v1.lpf_file_reader_pb2_grpc as lpf__file__reader__v1__pb2_grpc
 import ansys.api.speos.results.v1.lpf_file_reader_pb2 as lpf__file__reader__v1__pb2
-import ansys.data.speos.utils.v1.Point3_pb2 as Point3__v1__pb2
+#import ansys.data.speos.utils.v1.Point3_pb2 as Point3__v1__pb2
 
 class lpfreader:
     def __init__(self):
@@ -10,7 +10,7 @@ class lpfreader:
         self.stub = lpf__file__reader__v1__pb2_grpc.LpfFileReader_MonoStub(channel)
 
     def InitLpfFileName(self, path):
-        return self.stub.InitLpfFileName(lpf__file__reader__v1__pb2.InitLpfFileNameRequest_Mono(lpf_file_path=path))
-    
-
-
+        return self.stub.InitLpfFileName(
+            lpf__file__reader__v1__pb2.InitLpfFileNameRequest_Mono(lpf_file_path=path)
+            )
+            
