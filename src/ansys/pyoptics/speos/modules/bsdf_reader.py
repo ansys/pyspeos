272aa4c12881caf3a9eaf02db0b759f449483248
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-06-29 14:06:52 +0200

    remove static method

--- src/ansys/pyoptics/speos/modules/bsdf_reader.py
+++ src/ansys/pyoptics/speos/modules/bsdf_reader.py
@@ -1,27 +1,30 @@
 import ansys.api.speos.bsdf.v1.bsdf_creation_pb2 as bsdf_creation__v1__pb2
+import ansys.api.speos.bsdf.v1.bsdf_creation_pb2_grpc as bsdf_creation__v1__pb2_grpc
+from ansys.pyoptics import speos
 
 class bsdf_analysis():
+    def __init__(self, port):
+        self.port = port
+        self.stub = speos.get_stub_insecure_channel(
+            port=self.port, stub_type=bsdf_creation__v1__pb2_grpc.BsdfCreationServiceStub)
 
-    @staticmethod
-    def bsdf180(input_file_1, input_file_2, output_file):
+    def bsdf180(self, input_file_1, input_file_2, output_file):
         bsdf180_request = bsdf_creation__v1__pb2.Bsdf180InputData()
         bsdf180_request.input_front_bsdf_file_name = input_file_1
         bsdf180_request.input_opposite_bsdf_file_name = input_file_2
         bsdf180_request.output_file_name = output_file
-        return bsdf180_request
-    
-    @staticmethod
-    def spectral(input_file, output_file, wavelengths):
+        self.stub.BuildBsdf180(bsdf180_request)
+
+    def spectral(self, input_file, output_file, wavelengths):
         spectral_request = bsdf_creation__v1__pb2.SpectralBsdfInputData()
         for wavelength in wavelengths:
             tmp = spectral_request.input_anisotropic_samples.add()
             tmp.wavelength = wavelength
             tmp.file_name = input_file
         spectral_request.output_file_name = output_file
-        return spectral_request
+        self.stub.BuildSpectralBsdf(spectral_request)
 
-    @staticmethod
-    def Anisotropic(input_file, output_file, angles):
+    def Anisotropic(self, input_file, output_file, angles):
         anisotropic_request = bsdf_creation__v1__pb2.AnisotropicBsdfInputData()
         for angle in angles:
             temp = anisotropic_request.input_anisotropic_bsdf_samples.add()
@@ -29,4 +32,4 @@ class bsdf_analysis():
             temp.file_name = input_file
         anisotropic_request.fix_disparity = False
         anisotropic_request.output_file_name = output_file
-        return anisotropic_request
+        self.stub.BuildAnisotropicBsdf(anisotropic_request)
--- tests/test_bsdf_re.py
+++ tests/test_bsdf_re.py
@@ -13,9 +13,6 @@ With coverage.
 import math
 import os
 
-import ansys.api.speos.bsdf.v1.bsdf_creation_pb2_grpc as bsdf_creation__v1__pb2_grpc
-
-from ansys.pyoptics import speos
 from ansys.pyoptics.speos.modules.bsdf_reader import bsdf_analysis
 from conftest import config, test_path
 import helper
@@ -25,26 +22,27 @@ bsdf_output_file_name = os.path.join(test_path, "Test.bsdf180")
 tmp_file_name = os.path.join(test_path, "R_test.anisotropicbsdf")
 spectral_output_file_name = os.path.join(test_path, "Test.brdf")
 anisotropic_output_file_name = os.path.join(test_path, "Assembled.anisotropicbsdf")
-stub = speos.get_stub_insecure_channel(
-    port=config.get("SpeosServerPort"), stub_type=bsdf_creation__v1__pb2_grpc.BsdfCreationServiceStub
-)
+
+# bsdf file reader creation with port
+bsdf_request = bsdf_analysis(config.get("SpeosServerPort"))
 
 
 class Test_bsdf:
     def test_bsdf180(self):
-        bsdf_request = bsdf_analysis.bsdf180(input_file_name, input_file_name, bsdf_output_file_name)
-        stub.BuildBsdf180(bsdf_request)
-        assert helper.does_file_exist(bsdf_request.output_file_name)
-        helper.remove_file(bsdf_request.output_file_name)
+        bsdf_request.bsdf180(input_file_name, input_file_name, bsdf_output_file_name)
+        assert helper.does_file_exist(bsdf_output_file_name)
+        helper.remove_file(bsdf_output_file_name)
 
     def test_spectral(self):
-        spectral_request = bsdf_analysis.spectral(tmp_file_name, spectral_output_file_name, [400, 700])
-        stub.BuildSpectralBsdf(spectral_request)
-        assert helper.does_file_exist(spectral_request.output_file_name)
-        helper.remove_file(spectral_request.output_file_name)
+        bsdf_request.spectral(tmp_file_name, spectral_output_file_name, [400, 700])
+        assert helper.does_file_exist(
+            spectral_output_file_name,
+        )
+        helper.remove_file(
+            spectral_output_file_name,
+        )
 
     def test_anisotropic(self):
-        anisotropic = bsdf_analysis.Anisotropic(tmp_file_name, anisotropic_output_file_name, [0.0, math.pi / 2])
-        stub.BuildAnisotropicBsdf(anisotropic)
-        assert helper.does_file_exist(anisotropic.output_file_name)
-        helper.remove_file(anisotropic.output_file_name)
+        bsdf_request.Anisotropic(tmp_file_name, anisotropic_output_file_name, [0.0, math.pi / 2])
+        assert helper.does_file_exist(anisotropic_output_file_name)
+        helper.remove_file(anisotropic_output_file_name)
