1bb2e1c136e0402464f0f49a313ab16e3f42c430
Elodie Chamblas <elodie.chamblas@ansys.com>
2023-09-18 10:13:13 +0200

    update test with new method names

--- tests/test_client_spectrum.py
+++ tests/test_client_spectrum.py
@@ -25,12 +25,12 @@ def test_client_spectrum_init(client: SpeosClient):
     assert sBB5321.key() != ""
     assert sBB5321.database() is not None
     # Get data
-    sBB5321data = sBB5321.content()
+    sBB5321data = sBB5321.read_content()
     assert sBB5321data.blackbody.temperature == 5321
     # Update data
     sBB5321data.blackbody.temperature = 5326
-    sBB5321.set_content(sBB5321data)
-    sBB5321data = sBB5321.content()
+    sBB5321.update_content(sBB5321data)
+    sBB5321data = sBB5321.read_content()
     assert sBB5321data.blackbody.temperature == 5326
     # New from scratch spectrum
     sM659data = Spectrum.Content()
@@ -39,8 +39,8 @@ def test_client_spectrum_init(client: SpeosClient):
     sM659data.monochromatic.wavelength = 659
     SM659 = sDB.New(sM659data)
     # Duplicate
-    SM659_bis = sDB.New(SM659.content())
-    SM659_bisdata = SM659_bis.content()
+    SM659_bis = sDB.New(SM659.read_content())
+    SM659_bisdata = SM659_bis.read_content()
     assert SM659_bis.key() != SM659.key()
     assert SM659_bisdata == sM659data
     # Delete
