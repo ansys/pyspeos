ansys/pyspeos

commit 8154c6dfeede754b1facab9f9e6fb7e89bb0392f

build: bump the doc-deps group across 1 directory with 4 updates (#452)
Bumps the doc-deps group with 4 updates in the / directory:
[ansys-sphinx-theme](https://github.com/ansys/ansys-sphinx-theme),
[sphinx-autoapi](https://github.com/readthedocs/sphinx-autoapi),
[myst-parser](https://github.com/executablebooks/MyST-Parser) and
[jupytext](https://github.com/mwouts/jupytext).

Updates `ansys-sphinx-theme` from 1.1.6 to 1.3.1
<details>
<summary>Commits</summary>
<ul>
<li><a
href="https://github.com/ansys/ansys-sphinx-theme/commit/ef16f645ded6868b3538ee5e27e59c9682708e8e"><code>ef16f64</code></a>
chore: updating CHANGELOG for v1.3.1</li>
<li><a
href="https://github.com/ansys/ansys-sphinx-theme/commit/dc3d79c4ecd617d42f667156369fb10fc6fb03bd"><code>dc3d79c</code></a>
release: bump version 1.3.1</li>
<li><a
href="https://github.com/ansys/ansys-sphinx-theme/commit/ade8de723fb53e0bfdd8299aa898d8738b93f0aa"><code>ade8de7</code></a>
chore: updating CHANGELOG for v1.3.0</li>
<li><a
href="https://github.com/ansys/ansys-sphinx-theme/commit/9e7d05f959c925bf16f6a2f2f4b30f8b82d4fa94"><code>9e7d05f</code></a>
release: bump version 1.3.0</li>
<li><a
href="https://github.com/ansys/ansys-sphinx-theme/commit/ba7d06e62394dbd3270f9374e0b84b9d0e7ba0f9"><code>ba7d06e</code></a>
feat: add whatsnew options (<a
href="https://redirect.github.com/ansys/ansys-sphinx-theme/issues/583">#583</a>)</li>
<li><a
href="https://github.com/ansys/ansys-sphinx-theme/commit/bab85f5fc5f800eca532cf4757953d09de0cd825"><code>bab85f5</code></a>
chore: update CHANGELOG for v1.2.7 (<a
href="https://redirect.github.com/ansys/ansys-sphinx-theme/issues/613">#613</a>)</li>
<li><a
href="https://github.com/ansys/ansys-sphinx-theme/commit/6f9f59f60391407507f1135f868c3c0f05f67613"><code>6f9f59f</code></a>
fix: astroid version (<a
href="https://redirect.github.com/ansys/ansys-sphinx-theme/issues/612">#612</a>)</li>
<li><a

doc/source/conf.py
@@ -39,6 +39,7 @@ html_theme_options = {
     "check_switcher": False,
     "ansys_sphinx_theme_autoapi": {
         "project": project,
+        "own_page_level": "module",
     },
     "cheatsheet": {
         "file": "cheat_sheet/cheat_sheet_script.qmd",
@@ -63,7 +64,6 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx_design",
     "sphinx_jinja",
-    "sphinx.ext.autodoc",
     "ansys_sphinx_theme.extension.autoapi",
     "nbsphinx",
     "myst_parser",

pyproject.toml
@@ -48,16 +48,16 @@ jupyter = [
     "pyvista[jupyter]>=0.43,<0.45",
 ]
 doc = [
-    "ansys-sphinx-theme==1.1.6",
+    "ansys-sphinx-theme==1.3.1",
     "numpydoc==1.8.0",
     "Sphinx==8.1.3",
     "sphinx-copybutton==0.5.2",
-    "sphinx-autoapi==3.4.0",
+    "sphinx-autoapi==3.5.0",
     "sphinx_design==0.6.1",
     "sphinx-jinja==2.0.2",
-    "myst-parser==4.0.0",
+    "myst-parser==4.0.1",
     "nbsphinx==0.9.6",
-    "jupytext==1.16.6",
+    "jupytext==1.16.7",
     "jupyterlab==4.3.5",
     "jupyter-server==2.15.0",
     "nbconvert==7.16.6",

commit 450d6c3e04f581f112677f23b887a83b618352a3

docs: fix broken inner links (#465)
## Description
Following the breaking changes introduced in
https://github.com/ansys-internal/pyspeos/pull/452, this PR adds
templates for modules and classes to ensure that inner classes are
handled correctly.

## Issue linked
None

## Checklist
- [ ] I have tested my changes locally.
- [ ] I have added necessary documentation or updated existing
documentation.
- [ ] I have followed the coding style guidelines of this project.
- [ ] I have added appropriate tests (unit, integration, system).
- [ ] I have reviewed my changes before submitting this pull request.
- [ ] I have linked the issue or issues that are solved by the PR if
any.
- [ ] I have assigned this PR to myself.
- [ ] I have made sure that the title of my PR follows [Conventional
commits style](https://www.conventionalcommits.org/en/v1.0.0/#summary)
(e.g. ``feat: add optical property``)
- [ ] I have agreed with the Contributor License Agreement
([CLA](https://developer.ansys.com/form/cla-acceptance)).

---------

Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>
Co-authored-by: Revathy Venugopal <104772255+Revathyvenugopal162@users.noreply.github.com>

doc/source/conf.py
@@ -39,7 +39,6 @@ html_theme_options = {
     "check_switcher": False,
     "ansys_sphinx_theme_autoapi": {
         "project": project,
-        "own_page_level": "module",
     },
 }
 BUILD_CHEATSHEET = os.environ.get("BUILD_CHEATSHEET", "false").lower() == "true"

pyproject.toml
@@ -48,7 +48,7 @@ jupyter = [
     "pyvista[jupyter]>=0.43,<0.45",
 ]
 doc = [
-    "ansys-sphinx-theme==1.3.1",
+    "ansys-sphinx-theme==1.3.2",
     "numpydoc==1.8.0",
     "Sphinx==8.1.3",
     "sphinx-copybutton==0.5.2",