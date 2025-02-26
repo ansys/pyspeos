e9f0b513e7b98a2dcdc2203594361d4d0753c20f
sthoene <stefan.thoene@ansys.com>
2025-02-26 13:50:31 +0100

    fix D200

--- tests/core/test_spectrum.py
+++ tests/core/test_spectrum.py
@@ -20,9 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-"""
-Test basic using spectrums.
-"""
+"""Test basic using spectrums."""
 
 import os
 
