ff2a82a9c9bd10d4a7ad11545ea9c4bdde71295a
sthoene <stefan.thoene@ansys.com>
2025-02-26 13:51:42 +0100

    fix D210

--- tests/kernel/test_logging.py
+++ tests/kernel/test_logging.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-""" "Testing of log module."""
+"""Test log module."""
 
 import logging as deflogging  # Default logging
 import re
