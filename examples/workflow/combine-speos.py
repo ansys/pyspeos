42da8882c26943756be693a6f9850c41c275d33a
sthoene <stefan.thoene@ansys.com>
2025-03-18 09:24:57 +0100

    fix doc building and codestyle

--- examples/workflow/combine-speos.py
+++ examples/workflow/combine-speos.py
@@ -14,6 +14,7 @@ from ansys.speos.core.sensor import SensorCamera
 from ansys.speos.core.simulation import SimulationInverse
 from ansys.speos.core.source import SourceLuminaire
 from ansys.speos.core.workflow.combine_speos import SpeosFileInstance, combine_speos
+
 # -
 
 # ### Define constants
@@ -23,34 +24,48 @@ HOSTNAME = "localhost"
 GRPC_PORT = 50098  # Be sure the Speos GRPC Server has been started on this port.
 car_names = ["BlueCar", "RedCar"]
 environment_name = "Env_Simplified"
-USE_DOCKER = False  # Set to False if you're running this example locally as a Notebook.
+USE_DOCKER = True  # Set to False if you're running this example locally as a Notebook.
 USE_GPU = False
 
 # ## Coordinate systems
 #
 # Define the global coordinate systems for each of the assets.
 
-global_cs = [0, 0, 0,  # Origin
-             1, 0, 0,  # x-direction
-             0, 1, 0,  # y-direction
-             0, 0, 1,
-            ]
+global_cs = [
+    0,
+    0,
+    0,  # Origin
+    1,
+    0,
+    0,  # x-direction
+    0,
+    1,
+    0,  # y-direction
+    0,
+    0,
+    1,
+]
 car_cs = {}
-car_cs["red"] = [2000, 0, 35000,  # Origin of red car
-                 0.0, 0.0, -1.0,  # x-direction
-                 -1.0, 0.0, 0.0,  # y-direction
-                 0.0, 1.0, 0.0,   # z-direction
-                ]
-car_cs["blue"] = [2000, 0, 35000, 
-                  0.0, 0.0, -1.0, 
-                  -1.0, 0.0, 0.0, 
-                  0.0, 1.0, 0.0
-                 ]
+car_cs["red"] = [
+    2000,
+    0,
+    35000,  # Origin of red car
+    0.0,
+    0.0,
+    -1.0,  # x-direction
+    -1.0,
+    0.0,
+    0.0,  # y-direction
+    0.0,
+    1.0,
+    0.0,  # z-direction
+]
+car_cs["blue"] = [2000, 0, 35000, 0.0, 0.0, -1.0, -1.0, 0.0, 0.0, 0.0, 1.0, 0.0]
 
 
 # ## Load assets
-# Assets used to run this example are available in the 
-# [PySpeos repository](https://github.com/ansys/pyspeos/) on GitHub. 
+# Assets used to run this example are available in the
+# [PySpeos repository](https://github.com/ansys/pyspeos/) on GitHub.
 #
 # > **Note:** Make sure you
 # > have downloaded simulation assets and set ``test_data_path``
@@ -72,23 +87,26 @@ speos = Speos(host=HOSTNAME, port=GRPC_PORT)
 # - A blue car
 # - A red car
 
-full_env_path = tests_data_path / f'{environment_name}.speos' / f'{environment_name}.speos'
-car_paths = [tests_data_path / f'{car}.speos' / f'{car}.speos' for car in car_names]
-assets = [SpeosFileInstance(speos_file=str(full_env_path),
-                            axis_system=global_cs,
-                            ),  
-          SpeosFileInstance(
-                            speos_file=str(car_paths[0]),
-                            axis_system=car_cs["red"],
-                            ),  
-          SpeosFileInstance(
-                            speos_file=str(car_paths[1]),
-                            axis_system=car_cs["blue"],
-                            ),
-          ]
-p = combine_speos(speos=speos,
-                  speos_to_combine=assets,
-                 )
+full_env_path = tests_data_path / f"{environment_name}.speos" / f"{environment_name}.speos"
+car_paths = [tests_data_path / f"{car}.speos" / f"{car}.speos" for car in car_names]
+assets = [
+    SpeosFileInstance(
+        speos_file=str(full_env_path),
+        axis_system=global_cs,
+    ),
+    SpeosFileInstance(
+        speos_file=str(car_paths[0]),
+        axis_system=car_cs["red"],
+    ),
+    SpeosFileInstance(
+        speos_file=str(car_paths[1]),
+        axis_system=car_cs["blue"],
+    ),
+]
+p = combine_speos(
+    speos=speos,
+    speos_to_combine=assets,
+)
 
 print(p)
 
@@ -110,7 +128,7 @@ ssr = p.create_sensor(name="Camera.1", feature_type=SensorCamera)
 ssr.set_distortion_file_uri(
     uri=str(tests_data_path / "CameraInputFiles" / "CameraDistortion_190deg.OPTDistortion")
 ).set_mode_photometric().set_transmittance_file_uri(
-    uri=str(tests_data_path / "CameraInputFiles" /"CameraTransmittance.spectrum")
+    uri=str(tests_data_path / "CameraInputFiles" / "CameraTransmittance.spectrum")
 ).set_mode_color().set_red_spectrum_file_uri(
     uri=str(tests_data_path / "CameraInputFiles" / "CameraSensitivityRed.spectrum")
 ).set_blue_spectrum_file_uri(
@@ -129,7 +147,8 @@ ssr.commit()
 
 # +
 src = p.create_source(name="Luminaire.1", feature_type=SourceLuminaire)
-src.set_intensity_file_uri(uri=str(tests_data_path / "IES_C_DETECTOR.ies")
+src.set_intensity_file_uri(
+    uri=str(tests_data_path / "IES_C_DETECTOR.ies")
 ).set_spectrum().set_daylightfluorescent()
 src.set_axis_system([0, 10000, 50000, 1, 0, 0, 0, 1, 0, 0, 0, 1])
 
