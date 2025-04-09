3d489ecada6f59c334c27bf096711ae66a271f92
plu <plu@ansys.com>
2025-04-09 20:43:40 +0100

    Add cache saving and add vector calculation methods

--- src/ansys/speos/core/generic/general_methods.py
+++ src/ansys/speos/core/generic/general_methods.py
@@ -25,7 +25,9 @@
 this includes decorator and methods
 """
 
-from functools import wraps
+from functools import lru_cache, wraps
+import math
+from typing import List
 import warnings
 
 _GRAPHICS_AVAILABLE = None
@@ -70,6 +72,7 @@ def deprecate_kwargs(old_arguments: dict, removed_version="0.3.0"):
     return decorator
 
 
+@lru_cache
 def run_if_graphics_required(warning=False):
     """Check if graphics are available."""
     global _GRAPHICS_AVAILABLE
@@ -108,3 +111,58 @@ def graphics_required(method):
         return method(*args, **kwargs)
 
     return wrapper
+
+
+def magnitude_vector(vector: List[float]) -> float:
+    """
+    Compute the magnitude (length) of a 3D vector.
+
+    Parameters
+    ----------
+    vector: List
+        A 2D or 3D vector as a list [x, y, z].
+
+    Returns
+    -------
+    float
+        The magnitude (length) of the vector.
+    """
+    if len(vector) == 2:
+        # 2D vector magnitude
+        magnitude = math.sqrt(vector[0] ** 2 + vector[1] ** 2)
+    elif len(vector) == 3:
+        # 3D vector magnitude
+        magnitude = math.sqrt(vector[0] ** 2 + vector[1] ** 2 + vector[2] ** 2)
+    else:
+        raise ValueError("Input vector must be either 2D or 3D")
+
+    return magnitude
+
+
+def normalize_vector(vector: List[float]) -> List[float]:
+    """
+    Normalize a 2D or 3D vector to have a length of 1.
+
+    Parameters
+    ----------
+    vector: List
+        A vector as a list [x, y] for 2D or [x, y, z] for 3D.
+
+    Returns
+    -------
+    List
+        The normalized vector.
+    """
+    # Check if the vector has 2 or 3 components
+    magnitude = magnitude_vector(vector)
+    if magnitude == 0:
+        raise ValueError("Cannot normalize the zero vector")
+
+    if len(vector) == 2:
+        normalized_v = [vector[0] / magnitude, vector[1] / magnitude]
+    elif len(vector) == 3:
+        normalized_v = [vector[0] / magnitude, vector[1] / magnitude, vector[2] / magnitude]
+    else:
+        raise ValueError("Input vector must be either 2D or 3D")
+
+    return normalized_v
