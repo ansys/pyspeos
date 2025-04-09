d3077ffed3793a8df66cd6923b800ea0e1bf7978
plu <plu@ansys.com>
2025-04-09 20:39:55 +0100

    Add more methods in visualization_methods.py

--- src/ansys/speos/core/generic/visualization_methods.py
+++ src/ansys/speos/core/generic/visualization_methods.py
@@ -22,12 +22,201 @@
 
 """Provides the ``VisualData`` class."""
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
-from ansys.speos.core.generic.general_methods import graphics_required
+from ansys.speos.core.generic.general_methods import (
+    graphics_required,
+    magnitude_vector,
+    normalize_vector,
+)
 
 if TYPE_CHECKING:  # pragma: no cover
     import pyvista as pv
+
+
+@graphics_required
+class VisualCoordinateSystem:
+    """Visualization data for the coordinate system..
+
+    By default, there is empty visualization data.
+
+    Notes
+    -----
+    **Do not instantiate this class yourself**.
+    classes.
+    """
+
+    def __init__(self):
+        import pyvista as pv
+
+        self._origin = [0.0, 0.0, 0.0]
+        self._x_axis = pv.Arrow(
+            start=self._origin,
+            direction=[1.0, 0.0, 0.0],
+            scale=10.0,
+            tip_radius=0.05,
+            shaft_radius=0.01,
+        )
+        self._y_axis = pv.Arrow(
+            start=self._origin,
+            direction=[0.0, 1.0, 0.0],
+            scale=10.0,
+            tip_radius=0.05,
+            shaft_radius=0.01,
+        )
+        self._z_axis = pv.Arrow(
+            start=self._origin,
+            direction=[0.0, 0.0, 1.0],
+            scale=10.0,
+            tip_radius=0.05,
+            shaft_radius=0.01,
+        )
+
+    @property
+    def origin(self) -> List[float]:
+        """Returns the origin of the coordinate system.
+
+        Returns
+        -------
+        List[float]
+            The origin of the coordinate system.
+
+        """
+        return self._origin
+
+    @origin.setter
+    def origin(self, value: List[float]) -> None:
+        """Set the origin of the coordinate system.
+
+        Parameters
+        ----------
+        value: List[float]
+            The origin of the coordinate system.
+
+        Returns
+        -------
+        None
+        """
+        if len(value) != 3:
+            msg = "origin must be a list with three elements."
+            raise ValueError(msg)
+        self._origin = value
+
+    @property
+    def x_axis(self) -> "pv.Arrow":
+        """Returns the x-axis of the coordinate system.
+
+        Returns
+        -------
+        pv.Arrow
+            pyvista.Arrow the x-axis of the coordinate system.
+
+        """
+        return self._x_axis
+
+    @x_axis.setter
+    def x_axis(self, x_vector: List[float]) -> None:
+        """Set the x-axis of the coordinate system.
+
+        Parameters
+        ----------
+        x_vector: List[float]
+            The x-axis of the coordinate system.
+
+        Returns
+        -------
+        None
+        """
+        import pyvista as pv
+
+        if len(x_vector) != 3:
+            msg = "x_axis must be a list with three elements."
+            raise ValueError(msg)
+        self._x_axis = pv.Arrow(
+            start=self._origin,
+            direction=normalize_vector(vector=x_vector),
+            scale=magnitude_vector(vector=x_vector),
+            tip_radius=0.05,
+            shaft_radius=0.01,
+        )
+
+    @property
+    def y_axis(self) -> "pv.Arrow":
+        """
+        Returns the y-axis of the coordinate system.
+
+        Returns
+        -------
+        pv.Arrow
+            pyvista.Arrow the y-axis of the coordinate system.
+
+        """
+        return self._y_axis
+
+    @y_axis.setter
+    def y_axis(self, y_vector: List[float]) -> None:
+        """Set the y-axis of the coordinate system.
+
+        Parameters
+        ----------
+        y_vector: List[float]
+            The y-axis of the coordinate system.
+
+        Returns
+        -------
+        None
+        """
+        import pyvista as pv
+
+        if len(y_vector) != 3:
+            msg = "y_axis must be a list with three elements."
+            raise ValueError(msg)
+        self._y_axis = pv.Arrow(
+            start=self._origin,
+            direction=normalize_vector(vector=y_vector),
+            scale=magnitude_vector(vector=y_vector),
+            tip_radius=0.05,
+            shaft_radius=0.01,
+        )
+
+    @property
+    def z_axis(self) -> "pv.Arrow":
+        """
+        Returns the z-axis of the coordinate system.
+
+        Returns
+        -------
+        pv.Arrow
+            pyvista.Arrow the z-axis of the coordinate system.
+
+        """
+        return self._z_axis
+
+    @z_axis.setter
+    def z_axis(self, z_vector: List[float]) -> None:
+        """Set the z-axis of the coordinate system.
+
+        Parameters
+        ----------
+        z_vector: List[float]
+            The z-axis of the coordinate system.
+
+        Returns
+        -------
+        None
+        """
+        import pyvista as pv
+
+        if len(z_vector) != 3:
+            msg = "z_axis must be a list with three elements."
+            raise ValueError(msg)
+        self._z_axis = pv.Arrow(
+            start=self._origin,
+            direction=normalize_vector(vector=z_vector),
+            scale=magnitude_vector(vector=z_vector),
+            tip_radius=0.05,
+            shaft_radius=0.01,
+        )
 
 
 @graphics_required
@@ -38,132 +227,67 @@ class VisualData:
 
     Notes
     -----
-    **Do not instantiate this class yourself**, use set_dimensions method available in sensor
+    **Do not instantiate this class yourself**
     classes.
     """
 
-    def __init__(self) -> None:
-        self.data = pv.PolyData()
-        self.x_axis = pv.PolyData()
-        self.y_axis = pv.PolyData()
-        self.z_axis = pv.PolyData()
+    def __init__(self, coordinate_system=True):
+        import pyvista as pv
+
+        self._data = pv.PolyData()
+        self.coordinates = VisualCoordinateSystem() if coordinate_system else None
+        self.updated = False
 
     @property
-    def data(self) -> pv.PolyData:
+    def data(self) -> "pv.PolyData":
         """
-        Get visualization mesh data.
+        Returns the data of the sensor.
 
         Returns
         -------
         pv.PolyData
-            mesh data.
+            The data of the surface visualization.
 
         """
-        return self.data
+        return self._data
 
-    @property
-    def x_axis(self) -> pv.PolyData:
+    def add_data_triangle(self, triangle_vertices: List[List[float]]) -> None:
         """
-        Get x-axis arrow data.
-
-        Returns
-        -------
-        pv.PolyData
-            x-axis arrow data.
-
-        """
-        return self.x_axis
-
-    @property
-    def y_axis(self) -> pv.PolyData:
-        """
-        Get y-axis arrow data.
-
-        Returns
-        -------
-        pv.PolyData
-            y-axis arrow data.
-
-        """
-        return self.y_axis
-
-    @property
-    def z_axis(self) -> pv.PolyData:
-        """
-        Get z-axis arrow data.
-
-        Returns
-        -------
-        pv.PolyData
-            z-axis arrow data.
-
-        """
-        return self.z_axis
-
-    @data.setter
-    def data(self, value) -> None:
-        """
-        Set visualization data.
+        Add surface data triangle to Visualization data.
 
         Parameters
         ----------
-        value: pv.PolyData
-            visualization data.
+        triangle_vertices: List[List[float]]
+            The vertices of the triangle.
 
         Returns
         -------
         None
-
         """
-        self._data = value
+        import pyvista as pv
 
-    @x_axis.setter
-    def x_axis(self, value) -> None:
+        if len(triangle_vertices) != 3 or any(len(vertice) != 3 for vertice in triangle_vertices):
+            msg = "provided triangle vertices must have 3 elements"
+            raise ValueError(msg)
+        faces = [[3, 0, 1, 2]]
+        self._data = self._data.append_polydata(pv.PolyData(triangle_vertices, faces))
+
+    def add_data_rectangle(self, rectangle_vertices: List[List[float]]) -> None:
         """
-        Set x-axis arrow data.
+        Add surface data rectangle to Visualization data.
 
         Parameters
         ----------
-        value: pv.PolyData
-            x-axis arrow data.
+        rectangle_vertices: List[List[float]]
+            The vertices of the rectangle.
 
         Returns
         -------
         None
-
         """
-        self._x_axis = value
+        import pyvista as pv
 
-    @y_axis.setter
-    def y_axis(self, value) -> None:
-        """
-        Set y-axis arrow data.
-
-        Parameters
-        ----------
-        value: pv.PolyData
-            y-axis arrow data.
-
-        Returns
-        -------
-        None
-
-        """
-        self._y_axis = value
-
-    @z_axis.setter
-    def z_axis(self, value) -> None:
-        """
-        Set z-axis arrow data.
-
-        Parameters
-        ----------
-        value: pv.PolyData
-            z-axis arrow data.
-
-        Returns
-        -------
-        None
-
-        """
-        self._z_axis = value
+        if len(rectangle_vertices) != 3 or any(len(vertice) != 3 for vertice in rectangle_vertices):
+            msg = "provided rectangle vertices must have 3 elements"
+            raise ValueError(msg)
+        self._data = self._data.append_polydata(pv.Rectangle(rectangle_vertices))
