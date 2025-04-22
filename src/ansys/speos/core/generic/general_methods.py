5dcb7d23b733f10161b6f5caa8cc3094d9db09d6
plu <plu@ansys.com>
2025-04-22 23:34:48 +0100

    refactor the wavelength_to_rgb to general_methods.py

--- src/ansys/speos/core/generic/general_methods.py
+++ src/ansys/speos/core/generic/general_methods.py
@@ -25,6 +25,8 @@
 this includes decorator and methods
 """
 
+from __future__ import annotations
+
 from functools import lru_cache, wraps
 import os
 from pathlib import Path
@@ -227,3 +229,54 @@ def retrieve_speos_install_dir(
     if not speos_exec.is_file():
         error_no_install(speos_rpc_path, int(version))
     return speos_rpc_path
+
+
+def wavelength_to_rgb(wavelength: float, gamma: float = 0.8) -> [int, int, int, int]:
+    """Convert a given wavelength of light to an approximate RGB color value.
+
+    The wavelength must be given in nanometers in the range from 380 nm to 750 nm.
+    Based on the code from http://www.physics.sfasu.edu/astro/color/spectra.html
+
+    Parameters
+    ----------
+    wavelength : float
+        Wavelength in nanometer between 380-750 nm
+    gamma : float
+        Gamma value.
+        By default : ``0.8``
+    """
+    wavelength = float(wavelength)
+    if 380 <= wavelength <= 440:
+        attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
+        r = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
+        g = 0.0
+        b = (1.0 * attenuation) ** gamma
+    elif 440 <= wavelength <= 490:
+        r = 0.0
+        g = ((wavelength - 440) / (490 - 440)) ** gamma
+        b = 1.0
+    elif 490 <= wavelength <= 510:
+        r = 0.0
+        g = 1.0
+        b = (-(wavelength - 510) / (510 - 490)) ** gamma
+    elif 510 <= wavelength <= 580:
+        r = ((wavelength - 510) / (580 - 510)) ** gamma
+        g = 1.0
+        b = 0.0
+    elif 580 <= wavelength <= 645:
+        r = 1.0
+        g = (-(wavelength - 645) / (645 - 580)) ** gamma
+        b = 0.0
+    elif 645 <= wavelength <= 750:
+        attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
+        r = (1.0 * attenuation) ** gamma
+        g = 0.0
+        b = 0.0
+    else:
+        r = 0.0
+        g = 0.0
+        b = 0.0
+    r *= 255
+    g *= 255
+    b *= 255
+    return [int(r), int(g), int(b), 255]
