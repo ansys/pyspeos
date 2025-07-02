dcbb88af6acfe6079bce36937882f067d2398a69
plu <plu@ansys.com>
2025-07-02 11:08:23 +0100

    refactor the test code into example as temp location. use pathlib rather than os.path

--- examples/core/source_thermic_example_tmp.py
+++ examples/core/source_thermic_example_tmp.py
@@ -1,11 +1,11 @@
-import os
+from pathlib import Path
 
 import ansys.speos.core as core
 from ansys.speos.core.launcher import launch_local_speos_rpc_server
 import ansys.speos.core.source as source
 
 speos = launch_local_speos_rpc_server(version="252")
-speos_file = os.path.join(os.getcwd(), "Speos Bench", "Inverse.1.1.speos", "Inverse.1.1.speos")
+speos_file = Path.cwd() / "Speos Bench" / "Inverse.1.1.speos" / "Inverse.1.1.speos"
 project = core.Project(speos=speos, path=speos_file)
 mysource = project.find(name=".*", name_regex=True, feature_type=source.SourceThermic)
 mysource[0].set_emissive_faces_temp(value=100)
