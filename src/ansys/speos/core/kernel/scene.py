8bea2fbc4847db05ec04daf58d63c2b0454942bd
Sébastien Morais <sebastien.morais@ansys.com>
2026-04-09 11:10:12 +0200

    fix: default value

--- src/ansys/speos/core/kernel/scene.py
+++ src/ansys/speos/core/kernel/scene.py
@@ -113,7 +113,7 @@ class SceneLink(CrudItem):
             This is only necessary when the user protects the speos light box with a password.
         """
         if password is None:
-            password = os.getenv("PYSPEOS_ENCRYPTED_PASSWORD", "")
+            password = os.getenv("PYSPEOS_ENCRYPTED_PASSWORD", None)
         self._actions_stub.LoadFile(
             messages.LoadFile_Request(guid=self.key, file_uri=str(file_uri), password=password)
         )
